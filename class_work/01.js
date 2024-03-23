72aabbb282bc1886512e6eacb31855ebb16a72b1
user.email <chetanprakash7304@gmail.com>
2024-03-23 21:37:58 +0530

    Inital commit for first 2 lectures

---
+++ class_work/01.js
@@ -0,0 +1,9 @@
+function print_Riya()
+{
+    console.log("riya");
+}
+
+console.log("Namaste World");
+let interval = setInterval(print_Riya,3000);
+console.log("ok World");
+clearInterval(interval);
---
+++ class_work/02.js
@@ -0,0 +1,10 @@
+let obj = {
+    a : 1,
+    b : "Chetan",
+    c : {
+        gender: "male",
+        age : 20
+    }
+}
+let {x,y,z} = obj;
+console.log(x,y,z);
